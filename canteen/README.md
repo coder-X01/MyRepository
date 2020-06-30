43b5aac16950e7b57c955ae9372da15252d00f10
coder-X <691070437@qq.com>
2020-06-30 10:01:15 +0800

    1.还原拖动效果 2.调整上传成功跳转提示

--- canteen/README.md
+++ canteen/README.md
@@ -2,7 +2,7 @@
 
 > A Vue.js project
 
-## Build Setup1
+## Build Setup
 
 
 ``` bash
--- canteen/src/components/pages/myiframe.vue
+++ canteen/src/components/pages/myiframe.vue
@@ -1,75 +1,124 @@
 <template>
   <div class="accept-container">
-    <div class="back_box" @click="goBack">
-      返回
-    </div>
+    <vue-drag-resize
+      :parentLimitation="true"
+      :isResizable="false"
+      axis="y"
+      :y="200"
+      @clicked="startMove"
+      @dragstop="endMove"
+    >
+      <div class="back_box">返回</div>
+    </vue-drag-resize>
+
     <!-- <span class="go-back" v-show="goBackState">返回</span> -->
-  <iframe v-show="iframeState" style="position: absolute; left: 0" id="show-iframe" height="100%" width="100%" frameborder=0 name="showHere" scrolling=auto :src="URL"></iframe>
+    <iframe
+      v-show="iframeState"
+      style="position: absolute; left: 0"
+      id="show-iframe"
+      height="100%"
+      width="100%"
+      frameborder="0"
+      name="showHere"
+      scrolling="auto"
+      :src="URL"
+    ></iframe>
   </div>
 </template>
 
 <script>
-import { MessageBox } from 'mint-ui'
+import VueDragResize from "vue-drag-resize";
+import { MessageBox } from "mint-ui";
 export default {
-  name: 'myframe',
-  data () {
+  name: "myframe",
+  components: {
+    VueDragResize
+  },
+  data() {
     return {
-      URL: '',
+      URL: "",
       iframeState: true,
-      goBackState: true
-    }
+      goBackState: true,
+      sTime: null,
+      eTime: null
+    };
   },
   methods: {
-    goBack () {
-      console.log('父盒子内容')
-      this.$router.go(-1)
+    test() {
+      console.log("父盒子内容");
     },
-    init () {
-      let that = this
-      that.myModal.downLoad(that, true)
-      let oIframe = document.getElementById('show-iframe')
-      let deviceWidth = document.documentElement.clientWidth
-      let deviceHeight = document.documentElement.clientHeight
-      oIframe.style.width = deviceWidth + 'px'
-      oIframe.style.height = deviceHeight + 'px'
+    goBack() {
+      this.$router.go(-1);
+    },
+    init() {
+      let that = this;
+      that.myModal.downLoad(that, true);
+      let oIframe = document.getElementById("show-iframe");
+      let deviceWidth = document.documentElement.clientWidth;
+      let deviceHeight = document.documentElement.clientHeight;
+      oIframe.style.width = deviceWidth + "px";
+      oIframe.style.height = deviceHeight + "px";
       // console.log(this.$route.query)
-      let url = this.$route.query.target
-      // let url = 'http://baidu.com'
-      console.log(url)
+      // let url = this.$route.query.target
+      let url = "http://www.baidu.com";
+      console.log(url);
       if (!url) {
-        MessageBox.alert('功能正在开发中', '提示')
-        this.$router.push({path: '/home'})
+        MessageBox.alert("链接出错", "提示");
+        this.$router.push({ path: "/home" });
       } else {
-        this.URL = url
-        const iframe = document.querySelector('#show-iframe')
+        this.URL = url;
+        const iframe = document.querySelector("#show-iframe");
         iframe.onload = () => {
-          console.log('加载完成') // 这样每次都会触发
-          that.myModal.downLoad(that, false)
+          console.log("加载完成"); // 这样每次都会触发
+          that.myModal.downLoad(that, false);
+        };
+      }
+    },
+    startMove(evt) {
+      //   debugger;
+      this.sp = evt.currentTarget.offsetTop;
+      // this.sTime=null
+      this.eTime = null;
+      this.sTime = new Date();
+    },
+    endMove() {
+      if (this.sTime != null && this.eTime == null) {
+        this.eTime = new Date();
+        var diffTime = this.eTime - this.sTime;
+        // debugger;
+        if (diffTime < 2000) {
+          this.sTime = null;
+          this.eTime = null;
+          this.goBack();
         }
       }
     }
   },
-  mounted () {
-    this.init()
+  mounted() {
+    this.init();
   }
-}
+};
 </script>
 <style scoped>
-.back_box{
+.back_box {
   position: fixed;
-  top: 200px;
-  right: 0px;
+  right: 0;
   width: 150px;
   height: 70px;
   line-height: 70px;
-  background-color:rgba(120,120,120,0.3);
+  background-color: rgba(120, 120, 120, 0.3);
   border-radius: 40px 0 0 40px;
-  z-index: 1000;
+  z-index: 1;
 }
-.go-back{
+.go-back {
   position: fixed;
   top: 80px;
   right: 40px;
   z-index: 10;
+}
+
+.accept-container {
+  width: 100%;
+  height: 100%;
 }
 </style>
